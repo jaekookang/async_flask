7eeba70ddca1220b8126745ae0cf41ad7aaa5492
Jaekoo Kang <jaekoo.jk@gmail.com>
2019-04-15 23:44:43 -0400

    update async_mode

--- application.py
+++ application.py
@@ -16,8 +16,6 @@ Updated 13th April 2018
 """
 
 
-
-
 # Start with a basic flask app webpage.
 from flask_socketio import SocketIO, emit
 from flask import Flask, render_template, url_for, copy_current_request_context
@@ -32,12 +30,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = True
 
-#turn the flask app into a socketio app
-socketio = SocketIO(app)
+# turn the flask app into a socketio app
+socketio = SocketIO(app, async_mode='threading')
 
-#random number Generator Thread
+# random number Generator Thread
 thread = Thread()
 thread_stop_event = Event()
+
 
 class RandomThread(Thread):
     def __init__(self):
@@ -49,10 +48,10 @@ class RandomThread(Thread):
         Generate a random number every 1 second and emit to a socketio instance (broadcast)
         Ideally to be run in a separate thread?
         """
-        #infinite loop of magical random numbers
+        # infinite loop of magical random numbers
         print("Making random numbers")
         while not thread_stop_event.isSet():
-            number = round(random()*10, 3)
+            number = round(random() * 10, 3)
             print(number)
             socketio.emit('newnumber', {'number': number}, namespace='/test')
             sleep(self.delay)
@@ -63,8 +62,9 @@ class RandomThread(Thread):
 
 @app.route('/')
 def index():
-    #only by sending this page first will the client be connected to the socketio instance
+    # only by sending this page first will the client be connected to the socketio instance
     return render_template('index.html')
+
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
@@ -72,11 +72,12 @@ def test_connect():
     global thread
     print('Client connected')
 
-    #Start the random number generator thread only if the thread has not been started before.
+    # Start the random number generator thread only if the thread has not been started before.
     if not thread.isAlive():
         print("Starting Thread")
         thread = RandomThread()
         thread.start()
+
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
